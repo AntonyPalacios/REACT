92cb32b7fa4ab050048217f8caaf72a9a2a6689c
Antony Palacios Coronado <apalaciosc@pucp.edu.pe>
2024-12-14 09:40:48 -0500

    CURSO REACT 2022

---
+++ rendimiento/src/App.js
@@ -0,0 +1,40 @@
+import { useState, useCallback, useMemo } from 'react'
+import Title from './components/Title'
+import MyForm from './components/Forms/MyForm'
+import MyList from './components/Lists/MyList'
+
+function App() {
+  const [valores, setValores] = useState([])
+  const handleSubmit = useCallback((values) => {
+    setValores(data =>([
+      ...data,
+      values
+    ]))
+  },[])
+
+    console.time('memo')
+    /*
+    * 1. Una funcion de la cual va a guardar el resultado siempre y cuando los argumentos de estos no hayan cambiado
+    * 2. Argumentos que recibe la función
+    * */
+
+    const iterador = 50000000
+    const memoized = useMemo(()=>{
+        let total =  0
+        for(let i=0;i<iterador;i++){
+            total=total*iterador
+        }
+        return total
+    },[iterador])
+    console.timeEnd('memo')
+
+  return (
+    <div>
+      <Title>Mi título</Title>
+      <MyForm onSubmit={handleSubmit} />
+      <MyList data={valores} />
+    </div>
+  );
+}
+
+export default App;
